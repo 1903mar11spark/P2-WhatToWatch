d3d2d64c6ee94c5fd2e507e7d7b910dfebf90c37
Prutha <pruthapatel95@gmail.com>
2019-05-02 21:41:30 -0400

    DAO Impl and ConnectionUtil

--- WhatToWatch/src/main/java/com/project/dao/AdminDAO.java
+++ WhatToWatch/src/main/java/com/project/dao/AdminDAO.java
@@ -7,12 +7,14 @@ import com.project.beans.Credentials;
 import com.project.beans.Tier;
 import com.project.beans.User;
 
-public interface AdminDAO {//us, as employees
+public interface AdminDAO {
+	
+	//these methods will only be accessible to us as administrators 
 	
 	public Admin getAdminByCreds(Credentials creds);
-	public List<User> getAllUser(Admin admin);
-	public List<User> getAllByTier(Tier tier);
-	public boolean deleteUser(Admin admin);
+	public List<User> getAllUser(); 
+	public List<User> getAllUserByTier(Tier tier);
+	public boolean deleteUser(User user);
 	
 	
 
