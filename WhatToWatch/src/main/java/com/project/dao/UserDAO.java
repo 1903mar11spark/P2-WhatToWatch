789ddc5cc2e4c6e764394a0503c226309732718f
unknown <deborah.fwatson21@gmail.com>
2019-05-03 00:31:17 -0400

    Change to a .war file. had to use a different version of spring-context (5.1.3)

--- WhatToWatch/src/main/java/com/project/dao/UserDAO.java
+++ WhatToWatch/src/main/java/com/project/dao/UserDAO.java
@@ -1,5 +1,14 @@
 package com.project.dao;
 
+import com.project.beans.Credentials;
+import com.project.beans.User;
+
 public interface UserDAO {
+	
+	public User getUserByCreds(Credentials creds);
+	public boolean updateUser(User user);
+	public User createUser(User user);
+	public boolean delete(User user);
+	
 
 }
