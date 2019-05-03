3c3c1001dde5cdaa8a0b3904402a9fbbe4c41ebf
unknown <deborah.fwatson21@gmail.com>
2019-05-03 00:38:19 -0400

    changed to a war package, used a later version of spring-context

--- WhatToWatch/src/main/java/com/project/dao/AdminDAO.java
+++ WhatToWatch/src/main/java/com/project/dao/AdminDAO.java
@@ -7,14 +7,12 @@ import com.project.beans.Credentials;
 import com.project.beans.Tier;
 import com.project.beans.User;
 
-public interface AdminDAO {
-	
-	//these methods will only be accessible to us as administrators 
+public interface AdminDAO {//us, as employees
 	
 	public Admin getAdminByCreds(Credentials creds);
-	public List<User> getAllUser(); 
-	public List<User> getAllUserByTier(Tier tier);
-	public boolean deleteUser(User user);
+	public List<User> getAllUser(Admin admin);
+	public List<User> getAllByTier(Tier tier);
+	public boolean deleteUser(Admin admin);
 	
 	
 
