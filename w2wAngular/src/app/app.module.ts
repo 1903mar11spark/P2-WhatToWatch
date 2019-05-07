baf2dd2fb44d001364dbcecbde013030e98767a4
Shawn Currey <shawn.m.currey@gmail.com>
2019-05-07 02:35:19 -0400

    modifying login

--- w2wAngular/src/app/app.module.ts
+++ w2wAngular/src/app/app.module.ts
@@ -1,26 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component'
+import { AuthService } from './auth.service'
+import { UserService } from './user.service'
+import { AuthGuard } from './auth.guard';
+import { LogoutComponent } from './logout/logout.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     AdminComponent,
     HomeComponent,
-    LoginComponent,
     LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'logout',
+        component: LogoutComponent
+      },
+      {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [AuthService, UserService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
