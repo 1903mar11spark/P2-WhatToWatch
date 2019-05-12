375bd209de25688c846cf43a14657a832cea68d8
Shane Flynn <shanef3@gmail.com>
2019-05-12 17:46:16 -0400

    correct angular

---
+++ w2wAngular/src/app/content/wisecrack/wisecrack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wisecrack',
+  templateUrl: './wisecrack.component.html',
+  styleUrls: ['./wisecrack.component.css']
+})
+export class WisecrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
