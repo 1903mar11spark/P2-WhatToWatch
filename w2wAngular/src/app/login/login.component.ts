3a98ae61310e28b8f1d42ccf3986bcaa895b07f0
Shawn Currey <shawn.m.currey@gmail.com>
2019-05-12 20:41:51 -0400

    trying again

---
+++ w2wAngular/src/app/login/login.component.ts
@@ -0,0 +1,101 @@
+//import { Component, OnInit } from '@angular/core';
+//import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { LogincheckService } from './logincheck.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+//export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  username = 'test';
+  password = 'test';
+  public attempt = {};
+  constructor(private router: Router, private login: LogincheckService) { }
+
+  tryLogin(){
+    this.login.login(
+      this.username,
+      this.password
+    )
+    .subscribe(
+      data => { 
+        this.attempt = data;
+        console.log(data);
+        if(this.attempt == 'true'){
+          this.router.navigate(['/', 'user']);
+        }
+    })
+        //this.router.navigate(['/', 'user']);
+      //}
+    //}
+    
+  }
+ 
+
+ /*
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, AuthenticationService } from '../_services';
+
+@Component({templateUrl: 'login.component.html'})
+export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService
+    ) {
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) { 
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.authenticationService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                _data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+*/
+}
