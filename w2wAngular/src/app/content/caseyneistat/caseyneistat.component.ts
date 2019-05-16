9d08ace8843a1548f8ad9d930fbb3e9da5ddd7c4
Shawn Currey <shawn.m.currey@gmail.com>
2019-05-15 21:58:27 -0400

    omg yeet186482

---
+++ w2wAngular/src/app/content/caseyneistat/caseyneistat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caseyneistat',
+  templateUrl: './caseyneistat.component.html',
+  styleUrls: ['./caseyneistat.component.css']
+})
+export class CaseyneistatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ w2wAngular/src/app/content/prozd/prozd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prozd',
+  templateUrl: './prozd.component.html',
+  styleUrls: ['./prozd.component.css']
+})
+export class ProzdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
